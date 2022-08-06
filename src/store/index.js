2b143ddbf49f87f06c10e1a9d6868c5ceb90ff3f
Satheesh Reddy <satheeshreddy559@gmail.com>
2022-08-05 22:53:24 -0600

    react app with redux

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
